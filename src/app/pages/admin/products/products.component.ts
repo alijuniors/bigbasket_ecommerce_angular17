d585143c3944330294155e98492dc91258e60a80
alijuniors <75438240+alijuniors@users.noreply.github.com>
2024-02-26 10:39:03 -0400

    crud finalizado

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -6,6 +6,7 @@ import {AllproductsModule} from '../../../../modules/allproducts.module';
 import { ApiService } from '../../../../services/products/api.service';
 import { Observable } from 'rxjs';
 import { Product } from '../../../../modules/product';
+import { Categories } from '../../../../modules/categories';
 
 
 
@@ -19,36 +20,83 @@ import { Product } from '../../../../modules/product';
 export class ProductsComponent implements OnInit {
   isSidePanelVisible: boolean = false;
   productObj: any ={
-    "productId": 0,
-    "productSku": "",
-    "productName": "",
-    "productPrice": 0,
-    "productShortName": "",
-    "productDescription": "",
-    "createdDate": new Date(),
-    "deliveryTimeSpan": "",
-    "categoryId": 0,
-    "productImageUrl": ""
-
+    "id": "",
+    "title": "",
+    "price": 0,
+    "description": "",
+    "image": "",
+    "category":""
   };
   data : any[] = [];
+  products : any[] = [];
   constructor (private service: ApiService ){} 
-
+  getProducts(){
+    this.service.getProducts().subscribe(products =>{
+      this.products = products;
+    });
+  }
+  
   getData(){
-    this.service.getProduct().subscribe(data =>{
+    this.service.getCategories().subscribe(data =>{
       this.data = data;
-      const unique = [...new Set(data.map((item: any) => item.category))];
-      this.data = unique;
     });
   }
   
   ngOnInit(): void {
   this.getData(); 
+  this.getProducts(); 
   }
 
-  
- 
+  onSave() {
+    
+    this.service.saveProduct(this.productObj).subscribe((res:any)=>{
+      if(res.result) {
+        alert("product created")
+        this.getProducts();
+      }else{
+        alert(res.message);
+      }
+    })
+  }
 
+  onEdit(item: any){
+    this.productObj = item;
+    console.log(item);    
+    this.openSidePanel();
+    return false;
+  }
+  onUpdate(){
+   
+    this.service.updateProduct(this.productObj.id, this.productObj).subscribe((res:any)=>{
+      if(res.result) {
+        alert("product updated")
+        this.getProducts();
+      }else{
+        alert(res.message);
+      }
+    })
+
+
+  }
+  onDelete(id:any){  
+    const isDelete = confirm('Are you sure want to delete');
+    if (isDelete){
+    this.service.deleteProduct(id).subscribe((res:any)=>{
+      if(res.result) {
+        alert("product deleted")
+        this.getProducts();
+      }else{
+        alert(res.message);
+        
+      }
+    })
+    
+
+  }else {
+    alert("no se elimino");
+  }
+  return false;
+}
 
   openSidePanel(){
     this.isSidePanelVisible = true;
--- src/services/products/api.service.ts
+++ src/services/products/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Product } from '../../modules/product';
+import { Categories } from '../../modules/categories';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 @Injectable({
@@ -10,8 +11,20 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-    public getProduct():Observable<any>{
-      return this.http.get<any>(`http://fakestoreapi.com/products`);
+    public getCategories():Observable<any>{
+      return this.http.get<any>(`https://fakestoreapi.com/products/categories`);
+    }
+    public getProducts():Observable<any>{
+      return this.http.get<any>(`https://fakestoreapi.com/products`);
+    }
+    public saveProduct(obj: any):Observable<any>{
+      return this.http.post<any>(`https://fakestoreapi.com/products`,obj);
+    }
+    public updateProduct(id:any, obj: any):Observable<any>{
+      return this.http.put<any>(`https://fakestoreapi.com/products/`+ id,obj);
+    }
+    public deleteProduct(id:any):Observable<any>{
+      return this.http.delete<any>(`https://fakestoreapi.com/products/`+ id);
     }
 
     
